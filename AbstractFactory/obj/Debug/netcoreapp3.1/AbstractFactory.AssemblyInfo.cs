cd55e04094dfe0d4114f8fcd69765494dcd5f09e
Marcos Santos <marcoslapa@gmail.com>
2024-06-28 09:58:22 +0100

    Updating assemblyinfo

--- AbstractFactory/obj/Debug/netcoreapp3.1/AbstractFactory.AssemblyInfo.cs
+++ AbstractFactory/obj/Debug/netcoreapp3.1/AbstractFactory.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AbstractFactory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43ea3d34ba37842290a16feb4fc9c7e06e26968c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+419ca1512c18560f5a6cb75b18e95e8cdf0ab4f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("AbstractFactory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AbstractFactory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
